2af912ceb971b24e78450fc4bf5b7eed02343ba9
Brian Cassidy <brian.cassidy@gmail.com>
2015-06-01 10:19:26 -0300

    fix script name

--- route_request.sh
+++ route_request.sh
@@ -15,7 +15,7 @@ done
 # Triage if Pull Request or Deploy Build
 if [ -z "$ghprbPullId" ]; then
   # Deploy Build
-  /var/opt/github-drupal-deploy/deploy_remote.sh -u $URI_STRING
+  /var/opt/github-drupal-deploy/deploy_remote_full.sh -u $URI_STRING
 else
   # Pull Request, Build ShowCase
   /var/opt/github-drupal-deploy/build_showcase_local.sh -u $URI_STRING -p $WEBROOT -i $ghprbPullId
